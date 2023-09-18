8fe51a27946b9478a197a9d65bea56d467a816d5
RITHU M <rithunlr1@gmail.com>
2023-09-18 20:42:08 +0530

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Header />
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Dashboard
